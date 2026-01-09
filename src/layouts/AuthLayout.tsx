7e46729ec0fdb6453bed318fb672271c4caa03c8
Prabhashperera <prabashmperera01@gmail.com>
2026-01-10 01:22:31 +0530

    Design Changing v30

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 
 function AuthLayout() {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-[#0d0d12] p-4 text-white">
+        <div className="">
             <Outlet />
         </div>
     )
--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -4,7 +4,7 @@ import AmbientBgGlows from "../components/AmbientBgGlows"
 
 function HomeLayout() {
     return (
-        <div className="min-h-screen w-full bg-slate-950 font-sans text-white selection:bg-emerald-500/30 pb-20 overflow-x-hidden relative">
+        <div className="">
             <AmbientBgGlows />
             {/* <Navbar /> */}
             <Outlet />
